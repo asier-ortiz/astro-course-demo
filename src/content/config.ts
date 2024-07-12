cb3aa2b3a2753e35deaeb3b6c7f2ec0be69ce7df
Asier Ortiz García <asierortiz@outlook.es>
2024-07-12 11:22:33 +0200

    configuring content collections with zod

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,16 @@
-import {defineCollection} from "astro:content";
+import {defineCollection, z} from "astro:content";
+import {format} from "date-fns";
 
-const postsCollection = defineCollection({});
+const postsCollection = defineCollection({
+    schema: z.object({
+        author: z.string(),
+        categories: z.array(z.string()),
+        date: z.string().transform(str => format(new Date(str), 'MMMM d, yyyy')),
+        features: z.boolean().default(false),
+        image: z.string(),
+        title: z.string(),
+    }),
+});
 
 
 export const collections = {

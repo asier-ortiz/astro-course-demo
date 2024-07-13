937f185dec743f8f77cf242cb1e55ca9d37ed792
Asier Ortiz García <asierortiz@outlook.es>
2024-07-13 18:03:22 +0200

    setting up the astro image component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: 3_000
-  },
-  integrations: [tailwind()],
-  site: "https://rhythm.nation",
-
+    server: {
+        port: 3_000
+    },
+    integrations: [tailwind()],
+    site: "https://rhythm.nation",
 });

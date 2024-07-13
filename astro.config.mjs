c2525caf9703990e4fc178d4daaa660d938d7ef3
Asier Ortiz García <asierortiz@outlook.es>
2024-07-14 00:06:04 +0200

    adding Svelte and client directives

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import {defineConfig} from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
     server: {
         port: 3_000
     },
-    integrations: [tailwind()],
-    site: "https://rhythm.nation",
+    integrations: [tailwind(), svelte()],
+    site: "https://rhythm.nation"
 });
